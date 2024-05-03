50137a9ce6ffa80e074248c14d77dbaf6e737d7d
daniellberg <danielbergons@gmail.com>
2024-05-02 21:21:44 -0300

    new page

--- ToDoList/Sources/Controller/ListTaskController.swift
+++ ToDoList/Sources/Controller/ListTaskController.swift
@@ -9,8 +9,37 @@ import UIKit
 
 class ListTaskController: UIViewController {
 
+    //MARK: IBOutlet
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var btnAdd: UIButton!
+    
+    //MARK: Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
     }
     
+    //MARK: Action
+    @IBAction func createNewTask(_ sender: Any) {
+    }
+    
+}
+
+//MARK: UITableViewDataSource, UITableViewDelegate
+extension ListTaskController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: TaskTableCell = tableView.dequeueReusableCell(withIdentifier: "cellTask", for: indexPath) as! TaskTableCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 72
+    }
 }
---
+++ ToDoList/Sources/View/Cell/TaskTableCell.swift
@@ -0,0 +1,20 @@
+//
+//  TaskTableCell.swift
+//  ToDoList
+//
+//  Created by Daniel Berg on 02/05/24.
+//
+
+import UIKit
+
+class TaskTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
