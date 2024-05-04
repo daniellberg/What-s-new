8deda663a5dacf35cf6b8ddeb833b89e2b416713
daniellberg <danielbergons@gmail.com>
2024-05-03 22:01:49 -0300

    create time picker component and shwows empty task list

--- ToDoList/Sources/Controller/ManageTasksTableController.swift
+++ ToDoList/Sources/Controller/ManageTasksTableController.swift
@@ -14,11 +14,11 @@ class ManageTasksTableController: UITableViewController {
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 5
     }
 
 }
